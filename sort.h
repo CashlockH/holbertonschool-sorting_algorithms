c29523f001457970afa27d04c93da4df901c5103
CashlockH <ilyashesenli48@gmail.com>
2024-04-20 15:32:55 +0400

    prototype order is corrected

--- sort.h
+++ sort.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 /**
  * print_array - Prints an array of integers
@@ -45,20 +58,6 @@ void print_list(const listint_t *list)
 	}
 	printf("\n");
 }
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
